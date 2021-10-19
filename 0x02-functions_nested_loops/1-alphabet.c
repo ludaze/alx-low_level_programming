1fdf332af8c7a1e6de7894a853d27a0cfad75456
ludaze <luhanad@ymail.com>
2021-10-19 16:42:04 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print alphabet
+ *
+ *returns 0
+ */
+
+void print_alphabet(void)
+{
+  char a = 'a';
+
+  while (a <= 'z' )
+    {
+      _putchar(a);
+      ++a;
+    }
+  _putchar('\n');
+}
