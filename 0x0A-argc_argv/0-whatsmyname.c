21f2f95265a7abc6469b023eb28f188280309322
ludaze <56473410+ludaze@users.noreply.github.com>
2021-11-10 16:14:55 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
-*main - prints the name of this program.
-*Return: 0 on success.
-*/
-
+  *main - prints the  name of this program.
+  *@argc: counts number of arguemnts paased in th command line.
+  *@argv: stores arguments passed in the command line.
+  *
+  *Return: 0 on success.
+  */
 int main(int argc, char *argv[])
 {
-	printf("Name of the program is =%s\n",argv[0]);
-	return(0);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
