17f53105c0b78d1c47e59abc4200ab4979c2722c
ludaze <56473410+ludaze@users.noreply.github.com>
2021-11-10 17:13:53 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,27 +4,28 @@
   *main - prints result of multiplication followed by a new line.
   *@argc: number of arguments passed.
   *@argv: array of size argc containing arguments passed.
-  *@num1: the first number.
-  *@num2: the second number.
-  *@mul: the product.
   *
   *Return: 0 on success.
   *1 on error.
   */
 int main(int argc, char *argv[])
 {
-    int num1=argv[1];
-    int num2=argv[2];
-    int mul;
-    
-    if (argc==3)
-    {
-        mul= num1 * num2;
-        return (mul);
-    }
-    else
-    {
-        printf("Error\n");
-        return (1);
-    }
+	int count, mul;
+
+	mul = 1;
+	if (argc == 3)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			mul *= atoi(argv[count]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
 }
