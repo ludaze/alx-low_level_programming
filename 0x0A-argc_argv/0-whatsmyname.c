f013baedd8fcb70891c4479af7445a7d2b5cbfb3
ludaze <56473410+ludaze@users.noreply.github.com>
2021-11-10 16:10:16 +0300

    printing program file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - prints the name of this program.
+*Return: 0 on success.
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("Name of the program is =%s\n",argv[0]);
+	return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+working with the argc and argv . 
+learning the difference and their uses.
